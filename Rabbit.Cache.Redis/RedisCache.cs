d0c291776d8a6dc4e8726e490195981b5cb3b149
juanonsoftware <juanonsoftware@github.com>
2015-09-03 17:05:06 +0700

    Rabbit Cache V1

---
+++ Rabbit.Cache.Redis/RedisCache.cs
@@ -0,0 +1,36 @@
+﻿using Rabbit.Integrations.Redis;
+using StackExchange.Redis;
+using System;
+
+namespace Rabbit.Cache.Redis
+{
+    public class RedisCache : ICache
+    {
+        private readonly IDatabase _database;
+
+        public RedisCache(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public T Get<T>(string key) where T : class
+        {
+            return _database.Get<T>(key);
+        }
+
+        public bool Set<T>(string key, T value) where T : class
+        {
+            return _database.Set(key, value);
+        }
+
+        public bool Set<T>(string key, T value, TimeSpan expiry) where T : class
+        {
+            return _database.Set(key, value, expiry);
+        }
+
+        public bool Remove(string key)
+        {
+            return _database.KeyDelete(key);
+        }
+    }
+}
